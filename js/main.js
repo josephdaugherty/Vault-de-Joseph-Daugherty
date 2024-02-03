eee9b1842096d060f3cb0dcd674f7535d4162c48
josephdaugherty <daughe26@purdue.edu>
2024-02-02 22:46:01 -0500

    Format splash screen
    Temporarily disable splash screen for ease of use

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-var splashScreen = document.querySelector('.splash');
-splashScreen.addEventListener('click',()=>{
-  splashScreen.style.opacity = 0;
-  setTimeout(()=>{
-    splashScreen.classList.add('hidden')
-  },100)
-})
+// var splashScreen = document.querySelector('.splash');
+// splashScreen.addEventListener('click',()=>{
+//   splashScreen.style.opacity = 0;
+//   setTimeout(()=>{
+//     splashScreen.classList.add('hidden')
+//   },100)
+// })
+
+setTimeout(function() {
+  $('.splash').fadeOut(500);
+}, 1000); // <-- time in milliseconds
